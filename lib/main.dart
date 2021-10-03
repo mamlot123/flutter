66af918ebbbe98e2f15ef735338faea049552cb3
mamlot123 <mamlotbiswas1604199@gmail.com>
2021-10-03 11:52:55 +0600

    3 october 2021

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.purple,
-        title: Text(" *** Mamlot Biswas ***"),
+        title: Text(" *** Mamlot  ***"),
       ),
       body: RaisedButton(
         child: Text(btnText),
