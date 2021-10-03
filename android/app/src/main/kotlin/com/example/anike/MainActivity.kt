a7307bd341bee542b5df369ac8e636d3515be17f
unknown <mamlotbiswas1604199@gmail.com>
2021-10-03 11:02:35 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/anike/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.anike
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+String btnText = 'button';
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        title: Text("Mamlot Biswas"),
+      ),
+      body: RaisedButton(
+        child: Text(btnText),
+        onPressed: (){
+          setState(() {
+            btnText= 'button pressed';
+          });
+        },
+      ),
+    );
+  }
+}
